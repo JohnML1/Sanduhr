46389f64c06e6806d7526984998245e6f2d656ea
John Landmesser <jmlandmesser@gmail.com>
2020-12-12 16:44:13 +0100

    Add files via upload
    Neuer Timer misst Zusätzliche Zeit wenn zu messende  Zeit bereits vorbei

--- unit2.pas
+++ unit2.pas
@@ -6,18 +6,22 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  StdCtrls, LCLType;
+  StdCtrls, LCLType,  DateUtils;
 
 type
 
   { TFrmAlaram }
 
   TFrmAlaram = class(TForm)
+    Edit1 : TEdit;
     Label1: TLabel;
     Panel1: TPanel;
     Panel2: TPanel;
-    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    Timer1 : TTimer;
+    procedure FormKeyDown(Sender: TObject; var Key: word; Shift: TShiftState);
     procedure FormShow(Sender: TObject);
+    procedure Timer1StartTimer(Sender : TObject);
+    procedure Timer1Timer(Sender : TObject);
   private
 
   public
@@ -26,6 +30,9 @@ type
 
 var
   FrmAlaram: TFrmAlaram;
+  SecsCount1: integer = 0;
+  BlinkCount: integer = 0;
+
 
 implementation
 
@@ -35,10 +42,9 @@ uses unit1;
 
 { TFrmAlaram }
 
-procedure TFrmAlaram.FormKeyDown(Sender: TObject; var Key: Word;
-  Shift: TShiftState);
+procedure TFrmAlaram.FormKeyDown(Sender: TObject; var Key: word; Shift: TShiftState);
 begin
-  If Key = VK_RETURN then
+  if Key = VK_RETURN then
   begin
     FrmAlaram.Close;
     Application.Restore;
@@ -47,11 +53,50 @@ end;
 
 procedure TFrmAlaram.FormShow(Sender: TObject);
 begin
-  Panel2.Caption:= form1.EditZeitspanne.Caption;
+  Panel2.Caption := form1.EditZeitspanne.Caption;
   FrmAlaram.Top := 0;
-  FrmAlaram.Left:= 0;
+  FrmAlaram.Left := 0;
+  SecsCount1 := 0;
+  BlinkCount := 0;
+  FrmAlaram.Timer1.Enabled := true;
+end;
+
+procedure TFrmAlaram.Timer1StartTimer(Sender : TObject);
+begin
+  Edit1.Caption := '';
+end;
+
+procedure TFrmAlaram.Timer1Timer(Sender : TObject);
+var
+  val, SecsCountTime: TTime;
+  secs, secs1: int64;
+begin
+  (* globale Variable für verstrichene Sekunden erhöhen *)
+   inc(SecsCount1);
+
+  (* die verstrichenen Sekunden in TTime umrechnen
+    'OneSecond' ist in DateUtils definiet als Bruchteil eines Tages? *)
+  SecsCountTime := SecsCount1 * OneSecond;
+
+
+  inc(BlinkCount);
+
+  if BlinkCount <= 10 then
+  begin
+    Edit1.Font.Color := clRed;
+  end
+  else
+      Edit1.Font.Color := clDefault;
+
+  Edit1.Text := FormatDateTime('hh:nn:ss',SecsCountTime);
+
+
+
+
+
 end;
 
 
 end.
+
 
